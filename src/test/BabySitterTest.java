390b4caf58ccf5888540e5c5322f745df5345216
Abhinav <abhinav.vedanbhatla@gmail.com>
2016-04-21 01:04:44 -0400

    Refactoring

--- src/test/BabySitterTest.java
+++ src/test/BabySitterTest.java
@@ -2,70 +2,74 @@ package test;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import main.BabySitter;
 
 public class BabySitterTest {
-	
-	
+	BabySitter babySitter;
+	@Before
+	public void setUp(){
+		babySitter = new BabySitter();
+	}
 	@Test
 	public void ifStringIsInUpperCaseIntegerIsExpected(){
-		BabySitter babySitter = new BabySitter();
+		
 		assertEquals(17,babySitter.timeProcessing("5:00PM"));
 	}
 	
 	@Test
 	public void ifStringIsPmTheMethodAdds12AndReturnsInteger(){
-		BabySitter babySitter = new BabySitter();
+		
 		assertEquals(17,babySitter.timeProcessing("5:00PM"));
 	}
 	
 	@Test
 	public void ifStringIsAmTheMethodDoesNotAddAndReturnsInteger(){
-		BabySitter babySitter = new BabySitter();
+		
 		assertEquals(4,babySitter.timeProcessing("4:00AM"));
 	}
 	
 	@Test
 	public void ifStringIsNotInSpecifiedFormatItReturns13(){
-		BabySitter babySitter = new BabySitter();
+		
 		assertEquals(13,babySitter.timeProcessing("5:00GM"));
 	}
 	
 	@Test
 	public void ifThreeParametersAreGivenAsInputItReturnsInteger(){
-		BabySitter babySitter = new BabySitter();
+		
 		assertEquals(0,babySitter.salary(17,10,4));
 	}
 	
 	@Test
 	public void ifInTimeisEarlierThan5PMItReturnsInvalidEntry(){
-		BabySitter babySitter = new BabySitter();
+		
 		assertEquals(0,babySitter.salary(14, 11, 4));
 	}
 	
 	@Test
 	public void ifOutTimeisLaterThan4AMItReturnsInvalidEntry(){
-		BabySitter babySitter = new BabySitter();
+		
 		assertEquals(0,babySitter.salary(17, 11, 5));
 	}
 	
 	@Test
 	public void ifBedTimeisLessThanInTimeItReturnsInvalidEntry(){
-		BabySitter babySitter = new BabySitter();
+		
 		assertEquals(0,babySitter.salary(17, 3, 4));
 	}
 	
 	@Test
 	public void salaryMethodCheck(){
-		BabySitter babySitter = new BabySitter();
+		
 		assertEquals(140,babySitter.salary(17, 22, 4));
 	}
 	
 	@Test
 	public void fractionalHoursRoundedToFloorValue(){
-		BabySitter babySitter = new BabySitter();
+		
 		assertEquals(3,babySitter.timeProcessing("3:45aM"));
 	}
 }
